f378d8fe024bbdce6045eb458cdef19bde891620
Dom Sekotill <dom.sekotill@kodo.org.uk>
2023-01-28 21:27:28 +0000

    Export Message unions from top-level package

--- kilter/protocol/__init__.py
+++ kilter/protocol/__init__.py
@@ -10,7 +10,10 @@ Users looking for something as simple to use as libmilter should take a look at
 
 from typing import TYPE_CHECKING
 
+from .core import EditMessage as EditMessage
+from .core import EventMessage as EventMessage
 from .core import FilterProtocol as FilterProtocol
+from .core import ResponseMessage as ResponseMessage
 from .exceptions import *
 from .messages import *
 
